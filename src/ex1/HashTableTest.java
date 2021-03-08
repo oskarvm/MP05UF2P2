798645aa216ad45eef87a483ffe35f227ae5e78d
Oscar <ovazquez@elpuig.xeill.net>
2021-03-08 14:06:34 +0100

    Initial commit

---
+++ src/ex1/HashTableTest.java
@@ -0,0 +1,26 @@
+package ex1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @org.junit.jupiter.api.Test
+    void count() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void put() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void drop() {
+    }
+}
